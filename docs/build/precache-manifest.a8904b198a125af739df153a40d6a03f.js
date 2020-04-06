53ea8419cca224b14bbd781311c9a75aa2b3b4c9
Tuan Bui <ntuanb@gmail.com>
2020-04-06 16:36:38 +1000

    Add router to support base url

---
+++ docs/build/precache-manifest.a8904b198a125af739df153a40d6a03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bb40f05c604140d2dac6eda2e7eb7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9301c071f45d863ff163",
+    "url": "/static/css/main.b3dd3756.chunk.css"
+  },
+  {
+    "revision": "a222bdf8c94f0d2dca43",
+    "url": "/static/js/2.bb1fcbbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb1fcbbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9301c071f45d863ff163",
+    "url": "/static/js/main.f1517a35.chunk.js"
+  },
+  {
+    "revision": "195986249477019cc88c",
+    "url": "/static/js/runtime-main.631afa17.js"
+  }
+]);
